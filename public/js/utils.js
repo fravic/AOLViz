bac973415a90f577d8395d7be1c670a57dc0c91e
Fravic Fernando <fravicf@gmail.com>
2011-06-25 12:09:51 -0700

    fravic - New processing renderer

---
+++ public/js/utils.js
@@ -0,0 +1,20 @@
+$.fn.preload = function() {
+    this.each(function(){
+            var img = new Image();
+            img.src = this;
+            $(img).hide();
+            $("body").append($(img));
+        });
+};
+
+function shuffle(arr) {
+    var i = arr.length;
+    if ( i == 0 ) return false;
+    while ( --i ) {
+	var j = Math.floor( Math.random() * ( i + 1 ) );
+	var tempi = arr[i];
+	var tempj = arr[j];
+	arr[i] = tempj;
+	arr[j] = tempi;
+    }
+}
