b84e595d12ef2eca4edb459531bb19ec2ac17c1a
David Ma <ma.davidj@gmail.com>
2011-06-25 17:18:34 -0700

    preliminary sine waving stuff

--- server/static/js/snake.js
+++ server/static/js/snake.js
@@ -18,12 +18,18 @@ function _SnakeNode(time, pos, rad, color, stroke) {
     this.getRadius = function() {
         return _curRad;
     }
+    
+    this.getMaxRad = function() { return _maxRad; }
 
     this.getGlobalPos = function() {
         if (!_parent) {
             console.error("Parent not set on snake node!");
         }
         return addPoints(_pos, _parent.getGlobalPos());
+    }
+
+    this.setXPos = function(xpos) {
+        _pos.x = xpos;
     }
 
     this.setParent = function(parent) {
@@ -40,26 +46,41 @@ function _SnakeNode(time, pos, rad, color, stroke) {
     }
 
     function move() {
-        _pos.x += 1;
-        _pos.y += 1;
+        _pos.y = 25 * _parent.getAmpl() * Math.sin(1/(_parent.getWave() * 50) *
+                                               (_pos.x + _parent.getInitPos().x));
     }
 }
 
 function _Snake(speed, ampl, wave) {
 
+    console.log("new snake")
+
+    var _initpos = {x:Math.random()*Application.getWidth(),
+                    y:Math.random()*Application.getHeight()}
     var _speed = speed ? speed : 1;
     var _ampl = ampl ? ampl : 1;
     var _wave = wave ? wave : 1;
-    var _pos = {x:0, y:0};
+    var _pos = _initpos;
 
     var _nodes = [];
     var _nodeQueue = [];
+    var _nextNodeXPos = 0;
 
     this.addNode = function(snakeNode) {
+        console.log("new node")
+        console.log(_nextNodeXPos)
+        snakeNode.setXPos( _nextNodeXPos - snakeNode.getMaxRad());
+        _nextNodeXPos -= (snakeNode.getMaxRad() * 2)
         _nodeQueue.push(snakeNode);
     }
 
     this.getNodes = function() { return _nodes; }
+
+    this.getSpeed = function() { return _speed; }
+    this.getAmpl = function() { return _ampl; }
+    this.getWave = function() { return _wave; }
+    this.getGlobalPos = function() { return _pos; }
+    this.getInitPos = function() { return _initpos; }
 
     this.getGlobalPos = function() {
         return _pos;
@@ -84,5 +105,6 @@ function _Snake(speed, ampl, wave) {
     }
 
     function move() {
+        _pos.x += speed;
     }
 }
