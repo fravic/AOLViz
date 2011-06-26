5d40d7ebcfa93c5a5afd6e6b48fc5af57181dfd7
Lisa Zhang <lz.weiqi@gmail.com>
2011-06-26 10:02:29 -0700

    different colours

--- data/questionaes.py
+++ data/questionaes.py
@@ -1,12 +1,17 @@
 from web import get_files
 from math import log
 import time
-qword = {'who':1, 'what':2, 'when':3, 'where':4, 'why':5, 'how':6, 'could':7, 'should':8, 'would':9}
+qword = {'how':1, 'what':2, 'where':3, 'who':4, 'why':5, 'when':6, 'should':7, 'would':8, 'could':9}
 col1 = ["E41A1C", "377EB8", "4DAF4A", "984EA3", "FF7F00", "FFFF33", "A65628", "F781BF", "999999"]
 col2 = ["842326", "005589", "005700", "7E0187", "693F00", "4B4B00", "713900", "8B0067", "414E00"]
 
 col1 = [int(x, 16) for x in col1]
 col2 = [int(x, 16) for x in col2]
+
+PERCENT = {}
+for line in open('data/wc'):
+  word, cnt = line.strip().split('\t')
+  PERCENT[word] = float(cnt) / 1208165
 
 for line in get_files():
   if len(line) == 4:
@@ -25,4 +30,8 @@ for line in get_files():
 
   size = str(int(max(1,min(log(len(query), 1.4)-5,9))))
   ts = str(int(time.mktime(time.strptime(ts, '%Y-%m-%d %H:%M:%S'))))
-  print '\t'.join([id,ts,R,G,B,size, query])
+
+  tokens = query.split(' ')
+  percents = [PERCENT[token] for token in tokens]
+
+  print '\t'.join([id,ts,R,G,B,size, query, percents])
