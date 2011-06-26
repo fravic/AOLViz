1c3ae64b38e27de1cba037217073bb7964e0e4d8
Lisa Zhang <lz.weiqi@gmail.com>
2011-06-26 11:24:28 -0700

    make puke go away

--- data/questionaes.py
+++ data/questionaes.py
@@ -1,7 +1,7 @@
 from web import get_files
 from math import log
 import time
-qword = {'how':1, 'what':2, 'where':3, 'who':4, 'why':5, 'could':6, 'should':7, 'would':8, 'when':9}
+qword = {'how':1, 'what':2, 'where':3, 'who':4, 'would':5, 'could':6, 'should':7, 'why':8, 'when':9}
 col1 = ["E41A1C", "377EB8", "4DAF4A", "984EA3", "FF7F00", "FFFF33", "A65628", "F781BF", "999999"]
 col2 = ["720D0E", "1B3F5C", "265725", "4C2751", "7F3F00", "7F7F19", "222B14", "7B405F", "4C4C4C"]
 
